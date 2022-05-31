c07546288baeb41be5016946db3c52cc8aae5163
OmarZohir <46797225+OmarZohir@users.noreply.github.com>
2022-05-31 07:29:17 +0200

    Time: 42 ms (29.44%), Space: 10.9 MB (26.12%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // Solve as a sliding window problem (two sliding pointers)
+        int a_ptr = 0;
+        int b_ptr = 0;
+        int max_len = 0;
+        unordered_set<char> hashset;
+        
+        //Until the end of the 
+        while(b_ptr < s.length()){
+            //search for element in the hash map
+            if(hashset.find(s[b_ptr]) == hashset.end()){
+                hashset.insert(s[b_ptr]);
+                b_ptr++;
+                max_len = hashset.size() >= max_len ? hashset.size(): max_len;
+            }
+            else{
+                hashset.erase(s[a_ptr]);
+                a_ptr++;
+                
+            }
+        }
+        return max_len;
+    }
+};
