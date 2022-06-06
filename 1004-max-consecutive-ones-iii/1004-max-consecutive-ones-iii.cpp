3122f1cbe2c4757df774ae492773c11aec56d4a4
OmarZohir <46797225+OmarZohir@users.noreply.github.com>
2022-06-07 01:56:09 +0200

    Time: 64 ms (88.40%), Space: 55.4 MB (64.51%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int start, end;
-        start = end = 0;
         int max_length = 0;
-        int cur_length = 0;
-        while (end < nums.size()){
-            //if 1, increase count of max substr length
-           if(nums[end]){
-               cur_length++;
-               end++;
-           }
-            else{
-                //if  the digit is 0 and we have already flipped maximum number of 0s
-                if(k == 0){
-                    //check/unflip the previous first element
-                    if(nums[start] == 0){
-                        k++;
-                    }
-                    cur_length--;
-                    start++;
-                        
-                }
-                    
-                // if we have still some room for flipping 0s
-                else{
-                    cur_length++;
-                    k--;
-                    end++;
-                }     
+        for (int start = 0 ,end = 0, zeros = 0; end < nums.size(); end++){
+            //if it's zero
+            if(nums[end] == 0)
+                zeros++;
+            //if zeros > k, invalid window, shift from the left-hand side until you get a valid window
+            // that satisfies the condition
+            while(zeros > k){
+                if(nums[start] == 0)
+                    zeros--;
+                start++;
             }
-             max_length = max(max_length, cur_length);
+            max_length = max(max_length, end-start+1); // end-start+1 = length of the current window
         }
+       
         return max_length;
     }
 };
