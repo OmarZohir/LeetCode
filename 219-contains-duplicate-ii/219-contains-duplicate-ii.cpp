dc020bb451b9e83486315962dbe29939c15739e2
OmarZohir <46797225+OmarZohir@users.noreply.github.com>
2022-06-07 00:37:17 +0200

    Time: 200 ms (78.44%), Space: 72.1 MB (94.88%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(k==0) return false;
+        //count of number of elements in the window k
+        unordered_map<int, int> count_table_k;
+        // loop over the first k elements in the array from the left-hand side
+        for (int i = 0; i <= k && i < nums.size(); i++){
+            // if the element is seen for the first time, add it to the table
+            if(!count_table_k.count(nums[i]))
+                count_table_k.insert({nums[i],1});
+            else
+                //if the element has already been seen twice in that window, return true
+                return true;
+        }
+        
+        //end: end of the sliding window
+        //start moving the sliding window by step of 1
+        for(int end = k+1; end < nums.size(); end++){
+            //reduce the count for the element that WAS at the beginning of the sliding window 
+            count_table_k.erase(nums[end-k-1]);
+            //check if the new element to be added is already in the count table
+            if(count_table_k.count(nums[end]))
+                //if it's already in the table add it
+                return true;
+            else
+                //if not, add it
+                count_table_k.insert({nums[end],1});
+        }
+        return false;
+    }
+};
