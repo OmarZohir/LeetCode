3549f6fee8951670fe4a4397c6410e769ec5eaf2
OmarZohir <46797225+OmarZohir@users.noreply.github.com>
2024-05-20 03:49:49 +0300

    Time: 35 ms (12.53%), Space: 38.1 MB (63.13%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefix_prod;
-        vector<int> suffix_prod;
-        prefix_prod.resize(nums.size());
-        suffix_prod.resize(nums.size());
-        //initialize prefix prod start and suffix prod end
-        prefix_prod[0] = nums[0];
-        int end_idx = nums.size()-1; 
-        suffix_prod[end_idx] = nums[end_idx];
-        //get the prefix prod array
-        for (int i =1; i < nums.size(); ++i){
-            prefix_prod[i] = prefix_prod[i-1]*nums[i];
-            suffix_prod[end_idx-i] = suffix_prod[end_idx-i+1]*nums[end_idx-i];
+        vector<int> out;
+        out.resize(nums.size());
+
+        //prefix pass
+        // out[i] = pre[i-1] * post[i+1];
+        out[0] = 1;
+        int pre = nums[0];
+        for(int i = 1; i < nums.size(); ++i){
+            out[i] = pre;
+            pre*= nums[i];
         }
 
-        vector<int> output;
-        output.resize(nums.size());
-        output[0] = suffix_prod[1];
-        output[end_idx] = prefix_prod[end_idx-1];
-        for (int i=1; i < nums.size() -1; i++){
-            output[i] = prefix_prod[i-1]*suffix_prod[i+1];
-        }
-        return output;
+        //postfix pass
+        int post = 1;
+        for (int i = nums.size()-1; i >= 0; --i){
+            out[i] *=post;
+            post *= nums[i];
+        } 
+
+        return out;
     }
 };
